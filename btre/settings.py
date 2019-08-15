dd9434933c3da721d67bf262b58d62829bb86609
chimerablack1 <christopherparke@zoho.com>
2019-08-15 15:33:59 -0400

    added realtors model and migrations

--- btre/settings.py
+++ btre/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'btredb',
         'USER': 'postgres',
-        'PASSWORD': 'pass' ,
+        'PASSWORD': 'pass',
         'HOST':'localhost'
     }
 }
