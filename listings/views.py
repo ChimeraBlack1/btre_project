a9e5cba81f4dfd4592d4322eea1a2772ecd8d226
chimerablack1 <christopherparke@zoho.com>
2019-08-17 00:54:03 -0400

    preserving form input now

--- listings/views.py
+++ listings/views.py
@@ -58,12 +58,13 @@ def search(request):
         price = request.GET['price']
         if price:
             queryset_list = queryset_list.filter(price__lte=price)
-            
+
 
     context = {
         'state_choices': state_choices,
         'bedroom_choices': bedroom_choices,
         'price_choices': price_choices,
-        'listings': queryset_list
+        'listings': queryset_list,
+        'values': request.GET
     }
     return render(request,'listings/search.html', context)
